4f816dbaa282402572c55cce786fe891626938ad
onlyhereaccount <onlyhereaccount@126.com>
2016-01-21 21:58:47 +0800

    Revert "ddd"
    This reverts commit a0e7767d5fe04defb92f15f1f7fae49f461e7a75.
    
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ print 'hello github'
 
 print 'why I cant llllsee u?'
 
+<<<<<<< HEAD
 print 'I chankkkge cfxcfxcfu again??}}'
+=======
+print 'I chankkkge u again??'
+>>>>>>> parent of a0e7767... ddd
